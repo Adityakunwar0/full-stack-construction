bd6018a0b918123ef5c55cd99e35c060e74fdd5c
Aditya Kunwar <124910552+Adityakunwar0@users.noreply.github.com>
2026-05-26 15:47:24 +0545

    Fix typo in 'image' column name in articles table

--- backend/database/migrations/2026_05_04_043611_create_articles_table.php
+++ backend/database/migrations/2026_05_04_043611_create_articles_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('slug');
             $table->string('author')->nullable();
             $table->text('content')->nullable();
-            $table->string('iamge')->nullable();
+            $table->string('image')->nullable();
             $table->integer('status')->default(1);
             $table->timestamps();
         });
